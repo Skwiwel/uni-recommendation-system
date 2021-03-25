fa7784e8b262f2f2b0a1b2ab6e56126fb27ee771
Piotr Sawicki <sawickipiotr@outlook.com>
2021-03-25 22:14:11 +0100

    Added analysis of correlation of missing attributes with other attributes.

--- scripts/clean_data.py
+++ scripts/clean_data.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import os
+import datetime
 import numpy as np
 import pandas as pd
 
@@ -24,13 +25,18 @@ def clean_products_data():
 
 def clean_sessions_data():
     data = read_from_json(DATA_DIR_RAW+SESSIONS_FILENAME)
-    data = data.drop(columns=['offered_discount', 'purchase_id'])
     for index, row in data.iterrows():
         if pd.isna(row['user_id']):
             session_data = data.loc[(data['session_id'] == row['session_id']) & pd.notna(data['user_id'])]
             if session_data.size > 0:
                 data.loc[index, 'user_id'] = session_data['user_id'].values[0]
-    data = data.dropna()
+
+    if is_error_correlated(data, 'user_id') == False:
+        data.dropna(subset=['user_id'], inplace=True)
+    if is_error_correlated(data, 'product_id') == False:
+        data.dropna(subset=['product_id'], inplace=True)
+
+    data.drop(columns=['offered_discount', 'purchase_id'], inplace=True)
     save_to_json(data, DATA_DIR_TARGET+SESSIONS_FILENAME, DATA_DIR_TARGET)
 
 def read_from_json(filepath):
@@ -40,6 +46,24 @@ def save_to_json(df, filepath, create_dir=None):
     if create_dir is not None and not os.path.isdir(create_dir): 
         os.mkdir(create_dir)
     df.to_json(filepath, orient='records', date_format='iso', lines=True)
+
+def is_error_correlated(data: pd.DataFrame, column: str) -> bool:
+    column_ptr = column + '_ptr'
+    data.insert(len(data.columns), column_ptr, 0)
+    data.loc[data[column].isnull(), column_ptr] = 1
+
+    max_correlation = 0
+    for data_column in data.columns:
+        if data_column != column and data_column != column_ptr:
+            compare_with = data[data_column]
+            if data_column == 'timestamp':
+                compare_with = compare_with.transform(lambda x: datetime.datetime.strptime(x, "%Y-%m-%dT%H:%M:%S").timestamp())
+            elif data_column == 'event_type':
+                compare_with = compare_with.transform(lambda x: ord(x[0]))
+            max_correlation = max(max_correlation, abs(compare_with.corr(data[column_ptr])))
+
+    data.drop(column_ptr, 'columns', inplace=True)    
+    return max_correlation >= 0.1
 
 if __name__ == "__main__":
     clean_data()
